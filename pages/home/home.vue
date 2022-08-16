c0963a59f541a3bc855de300c8707d2ac65e3451
wsf1992 <741987899@qq.com>
2022-08-16 18:33:09 +0800

    fix: 接口请求

--- pages/home/home.vue
+++ pages/home/home.vue
@@ -14,24 +14,26 @@
 				<view class="f-s-18 c-999">></view>
 			</view>
 		</view>
-		<view class="w-flex-column">
+		<view class="w-flex-column mar-b-10">
 			<view class="city-title">热门城市</view>
-			<view class="uni-flex" style="flex-wrap:wrap;border-bottom: 1px solid #e4e4e4;">
-				<view class="city-item">1</view>
-				<view class="city-item">1</view>
-				<view class="city-item">1</view>
-				<view class="city-item">1</view>
-				<view class="city-item">1</view>
-				<view class="city-item">1</view>
-				<view class="city-item">1</view>
-				<view class="city-item">1</view>
-				<view class="city-item">1</view>
-				<view class="city-item">1</view>
-				<view class="city-item">1</view>
-				<view class="city-item">1</view>
-				<view class="city-item">1</view>
+			<navigator url="#A"></navigator>
+			<a href="#A"></a>
+			<view class="uni-flex" style="flex-wrap:wrap;overflow:hidden;">
+				<view class="city-item" v-for="item in hotList" :key="item.id">{{item.name}}</view>
 			</view>
 		</view>
+		<template v-for="item in groupList" :key="item.title">
+			<view class="w-flex-column mar-b-10">
+				<view class="city-title" name="item.title">{{item.title}}</view>
+					<view class="uni-flex" style="flex-wrap:wrap;overflow:hidden;">
+						<view class="city-item"  v-for="(obj, i) in item.items" :key="i">{{obj.name}}</view>
+					</view>
+			</view>
+		</template>
+		<view class="anchor-list">
+			<view v-for="item in indexList" :key="item" @click="jump(item)">{{item}}</view>
+		</view>
+		
 	</view>
 </template>
 
@@ -39,11 +41,55 @@
 	export default {
 		data() {
 			return {
-				
+				hotList: [],
+				groupList: [],
+				indexList: []
 			}
 		},
 		methods: {
-			
+			getHot() {
+				uni.request({
+					url: 'http://120.48.75.81:8001/v1/cities',
+					data: {
+						type: 'hot'
+					},
+					success: res => {
+						this.hotList = res.data
+					},
+					fail: err => {
+						console.log('获取热门城市失败', err)
+					}
+				})
+			},
+			getGroupCity() {
+				uni.request({
+					url: 'http://120.48.75.81:8001/v1/cities',
+					data: {
+						type: 'group'
+					},
+					success: res => {
+						for(let key in res.data) {
+							this.groupList.push({"title": key, "items": res.data[key]})
+							this.indexList.push(key)
+						}
+						this.groupList.sort(function(a, b) {
+								return a.title.charCodeAt(0)-b.title.charCodeAt(0)
+							})
+						this.indexList.sort()
+					},
+					fail: err => {
+						console.log('获取城市分组失败', err)
+					}
+				})
+			},
+			jump(href) {
+				console.log(11111111, `#${href}`)
+				location.href = `#${href}`
+			}
+		},
+		created() {
+			this.getHot()
+			this.getGroupCity()
 		}
 	}
 </script>
@@ -89,5 +135,15 @@
 }
 .city-item:nth-child(4n) {
 	border-right-width: 0px;
+}
+.anchor-list {
+	position:fixed;
+	right:0px;
+	top:50%;
+	margin-top: -209px;
+	font-size: 14px;
+	width: 22px;
+	text-align:center;
+	color: #666;
 }
 </style>
