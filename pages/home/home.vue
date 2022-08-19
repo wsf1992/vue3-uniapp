f3b968e6bdd2a46e96fa4be867b0b424c946e949
wsf1992 <741987899@qq.com>
2022-08-19 17:22:22 +0800

    fix: 添加列表页面自动跳转到 锚点功能

--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,5 +1,5 @@
 <template>
-	<view class="w-flex-column">
+	<view class="w-flex-column" id="box">
 		<view class="uni-flex w-flex-jusify-between w-header">
 			<view>ele.me</view>
 			<view>登陆|注册</view>
@@ -16,82 +16,104 @@
 		</view>
 		<view class="w-flex-column mar-b-10">
 			<view class="city-title">热门城市</view>
-			<navigator url="#A"></navigator>
-			<a href="#A"></a>
 			<view class="uni-flex" style="flex-wrap:wrap;overflow:hidden;">
-				<view class="city-item" v-for="item in hotList" :key="item.id">{{item.name}}</view>
+				<view class="city-item" v-for="item in hotList" :key="item.id">{{ item.name }}</view>
 			</view>
 		</view>
 		<template v-for="item in groupList" :key="item.title">
 			<view class="w-flex-column mar-b-10">
-				<view class="city-title" name="item.title">{{item.title}}</view>
-					<view class="uni-flex" style="flex-wrap:wrap;overflow:hidden;">
-						<view class="city-item"  v-for="(obj, i) in item.items" :key="i">{{obj.name}}</view>
-					</view>
+				<view class="city-title" :name="item.title" :id="item.title">{{ item.title }}</view>
+				<view class="uni-flex" style="flex-wrap:wrap;overflow:hidden;">
+					<view class="city-item" v-for="(obj, i) in item.items" :key="i">{{ obj.name }}</view>
+				</view>
 			</view>
 		</template>
 		<view class="anchor-list">
-			<view v-for="item in indexList" :key="item" @click="jump(item)">{{item}}</view>
+			<view v-for="item in indexList" :key="item" @click.prevent="jump(item)">{{ item }}</view>
 		</view>
-		
 	</view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				hotList: [],
-				groupList: [],
-				indexList: []
-			}
+export default {
+	data() {
+		return {
+			hotList: [],
+			groupList: [],
+			indexList: []
+		};
+	},
+	methods: {
+		getHot() {
+			uni.request({
+				url: 'http://120.48.75.81:8001/v1/cities',
+				data: {
+					type: 'hot'
+				},
+				success: res => {
+					this.hotList = res.data;
+				},
+				fail: err => {
+					console.log('获取热门城市失败', err);
+				}
+			});
 		},
-		methods: {
-			getHot() {
-				uni.request({
-					url: 'http://120.48.75.81:8001/v1/cities',
-					data: {
-						type: 'hot'
-					},
-					success: res => {
-						this.hotList = res.data
-					},
-					fail: err => {
-						console.log('获取热门城市失败', err)
+		getGroupCity() {
+			uni.request({
+				url: 'http://120.48.75.81:8001/v1/cities',
+				data: {
+					type: 'group'
+				},
+				success: res => {
+					for (let key in res.data) {
+						this.groupList.push({ title: key, items: res.data[key] });
+						this.indexList.push(key);
 					}
-				})
-			},
-			getGroupCity() {
-				uni.request({
-					url: 'http://120.48.75.81:8001/v1/cities',
-					data: {
-						type: 'group'
-					},
-					success: res => {
-						for(let key in res.data) {
-							this.groupList.push({"title": key, "items": res.data[key]})
-							this.indexList.push(key)
-						}
-						this.groupList.sort(function(a, b) {
-								return a.title.charCodeAt(0)-b.title.charCodeAt(0)
-							})
-						this.indexList.sort()
-					},
-					fail: err => {
-						console.log('获取城市分组失败', err)
-					}
-				})
-			},
-			jump(href) {
-				console.log(11111111, `#${href}`)
-				location.href = `#${href}`
-			}
+					this.groupList.sort(function(a, b) {
+						return a.title.charCodeAt(0) - b.title.charCodeAt(0);
+					});
+					this.indexList.sort();
+				},
+				fail: err => {
+					console.log('获取城市分组失败', err);
+				}
+			});
 		},
-		created() {
-			this.getHot()
-			this.getGroupCity()
+		jump(href) {
+			uni.createSelectorQuery()
+				.select('#box')
+				.boundingClientRect(bData => {
+					uni.createSelectorQuery()
+						.select(`#${href}`)
+						.boundingClientRect(data => {
+							let sctop = 0;
+							if (data.top < 0) {
+								if (bData.top < 0) {
+									sctop = data.top - bData.top;
+								} else {
+									sctop = bData.top - data.top;
+								}
+							} else {
+								if (bData.top < 0) {
+									sctop = data.top - bData.top;
+								} else {
+									sctop = bData.top + data.top;
+								}
+							}
+							uni.pageScrollTo({
+								scrollTop: sctop
+							});
+						})
+						.exec();
+				})
+				.exec();
 		}
+	},
+	created() {
+		this.getHot();
+		this.getGroupCity();
 	}
+};
 </script>
 
 <style>
@@ -118,9 +140,8 @@
 	height: 36px;
 	line-height: 36px;
 	border-top: 2px solid #e4e4e4;
-	border-bottom:1px solid #e4e4e4;
+	border-bottom: 1px solid #e4e4e4;
 	padding-left: 11px;
-
 }
 .city-item {
 	box-sizing: border-box;
@@ -137,13 +158,15 @@
 	border-right-width: 0px;
 }
 .anchor-list {
-	position:fixed;
-	right:0px;
-	top:50%;
+	position: fixed;
+	right: 0px;
+	top: 50%;
 	margin-top: -209px;
 	font-size: 14px;
 	width: 22px;
-	text-align:center;
+	text-align: center;
 	color: #666;
+	background-color: #e4e4e4;
+	opacity: 0.5;
 }
 </style>
