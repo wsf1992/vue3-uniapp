ae3fdd84d11667251b62ab7b9115fd0691a3b2d0
wsf1992 <741987899@qq.com>
2022-09-05 21:30:52 +0800

    fix: tips-pop 组件修改 setup 写法

--- components/tips-pop/tipsPop.vue
+++ components/tips-pop/tipsPop.vue
@@ -1,38 +1,46 @@
 <template>
 	<uni-popup ref="popup" :mask-click="false">
-		<view class="pop-box w-flex-column w-flex-cross-center">
-			<uni-icons type="info" size="100" color="#f8cb86"></uni-icons>
-			<text>{{ popTxt }}</text>
-		</view>
-		<button type="primary" class="pop-btn" @click="submit">确定</button>
+			<view class="pop-box w-flex-column w-flex-cross-center">
+				<uni-icons type="info" size="100" color="#f8cb86"></uni-icons>
+				<text class="text-center">{{ popTxt }}</text>
+			</view>
+			<button type="primary" class="pop-btn" @click="submit">确定</button>
 	</uni-popup>
 </template>
 
 <script setup>
-import { ref, defineProps } from 'vue';
+import {ref, defineExpose, onMounted } from 'vue';
 
 const callback = ref(null);
+const popup = ref()
 
 defineProps({
 	popTxt: String
 });
 
 function openPop() {
-	this.$refs.popup.open('center');
+	popup.value.open('center');
 }
 function submit() {
-	this.callback();
+	callback.value();
 }
+defineExpose({
+	callback,
+	openPop
+})
 </script>
 
 <style scoped>
 .pop-box {
+	width: 130px;
 	padding: 10px 30px;
 	background-color: #fff;
-	border-radius: 6px 6px 2px 2px;
+	border-radius: 6px 6px 0px 0px;
 }
 .pop-btn {
 	width: 100%;
 	background-color: #4cd964;
+	border-top-right-radius: 0;
+	border-top-left-radius: 0;
 }
 </style>
