e5f2e09883b054af60b11f8213df9b5e3062635c
wsf1992 <741987899@qq.com>
2022-09-05 22:52:42 +0800

    fix: 替换 setup 写法

--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,8 +1,11 @@
 <template>
 	<view class="w-flex-column" id="box">
-		<uni-nav-bar fixed backgroundColor="#3190e8" color="#fff" @clickTitle="clickHeaderTitle" status-bar title="ele.me" leftWidth="120px" rightWidth="120px">
+		<uni-nav-bar fixed backgroundColor="#3190e8" color="#fff" @clickTitle="clickHeaderTitle" status-bar
+			title="ele.me" leftWidth="120px" rightWidth="120px">
 			<template v-slot:left>
-				<navigator url="/pages/tabBar/profile/profile" v-if="user_id" open-type="switchTab"><uni-icons type="person" size="25px" color="#fff"></uni-icons></navigator>
+				<navigator url="/pages/tabBar/profile/profile" v-if="user_id" open-type="switchTab">
+					<uni-icons type="person" size="25px" color="#fff"></uni-icons>
+				</navigator>
 				<navigator url="/pages/login/login" v-else><text class="f-s-14">登录 | 注册</text></navigator>
 			</template>
 		</uni-nav-bar>
@@ -12,7 +15,8 @@
 				<text class="f-s-12 f-w-900 c-9f9f9f">定位不准时，请在城市列表中选择</text>
 			</template>
 		</uni-section>
-		<uni-section class="cur-city" :title="curCity.name" titleFontSize="18px" titleColor="#3190e8" @click="toCity(curCity.id)">
+		<uni-section class="cur-city" :title="curCity.name" titleFontSize="18px" titleColor="#3190e8"
+			@click="toCity(curCity.id)">
 			<template v-slot:right>
 				<text class="c-9f9f9f">></text>
 			</template>
@@ -31,7 +35,8 @@
 		</view>
 		<template v-for="item in groupList" :key="item.title">
 			<view class="w-flex-column mar-b-10">
-				<uni-section class="border-t" :id="item.title" :title="item.title" titleFontSize="14px" titleColor="#666"></uni-section>
+				<uni-section class="border-t" :id="item.title" :title="item.title" titleFontSize="14px"
+					titleColor="#666"></uni-section>
 				<uni-grid :column="4" borderColor="#e4e4e4" :square="false">
 					<template v-for="(item, i) in item.items" :key="i">
 						<uni-grid-item class="city-item">
@@ -49,146 +54,166 @@
 	</view>
 </template>
 
-<script>
-import { getGuessCity, getHot, getGroupCity, getUserInfo } from '@/common/fetch.js';
-export default {
-	data() {
-		return {
-			hotList: [],
-			groupList: [],
-			indexList: [],
-			curCity: '',
-			user_id: ''
-		};
-	},
-	methods: {
-		getGuessCity() {
-			getGuessCity().then(res => {
-				this.curCity = res.data;
-			});
-		},
-		getHot() {
-			getHot().then(res => {
-				this.hotList = res.data;
-			});
-		},
-		getGroupCity() {
-			console.log('开始请求', new Date().getTime());
-			getGroupCity().then(res => {
-				console.log('数据获取成功', new Date().getTime());
-				let group = [],
-					index = [];
-				for (let key in res.data) {
-					group.push({ title: key, items: res.data[key] });
-					index.push(key);
-				}
-				group.sort(function(a, b) {
-					return a.title.charCodeAt(0) - b.title.charCodeAt(0);
-				});
-				this.groupList = group;
-				console.log('数据处理完成', new Date().getTime());
-				index.sort();
-				this.index = index;
-			});
-		},
-		clickHeaderTitle() {
-			uni.pageScrollTo({
-				scrollTop: 0
-			});
-		},
-		jump(href) {
-			uni.createSelectorQuery()
-				.select('#box')
-				.boundingClientRect(bData => {
-					uni.createSelectorQuery()
-						.select(`#${href}`)
-						.boundingClientRect(data => {
-							let sctop = 0;
-							if (data.top < 0) {
-								if (bData.top < 0) {
-									sctop = data.top - bData.top;
-								} else {
-									sctop = bData.top - data.top;
-								}
-							} else {
-								if (bData.top < 0) {
-									sctop = data.top - bData.top;
-								} else {
-									sctop = bData.top + data.top;
-								}
-							}
-							uni.pageScrollTo({
-								scrollTop: sctop - 88
-							});
-						})
-						.exec();
-				})
-				.exec();
-		},
-		toCity(id) {
-			uni.navigateTo({
-				url: `/pages/city/city?id=${id}`
-			});
-		},
-		getUserInfo() {
-			const user_id = uni.getStorageSync('user_id');
-			getUserInfo(user_id).then(res => {
-				console.log(123, res);
-				if (res.data.user_id) this.user_id = res.data.user_id;
-			});
-		}
-	},
-	created() {
-		this.getUserInfo();
-		this.getHot();
-		this.getGroupCity();
-		this.getGuessCity();
+<script setup>
+	import {
+		getGuessCity,
+		getHot,
+		getGroupCity,
+		getUserInfo
+	} from '@/common/fetch.js';
+
+	import {
+		ref,
+		onMounted
+	} from 'vue'
+
+	const user_id = ref('')
+
+	function getUser() {
+		const local_user_id = uni.getStorageSync('user_id');
+		getUserInfo(local_user_id).then(res => {
+			console.log(123, res);
+			if (res.data.user_id) user_id.value = res.data.user_id;
+		});
 	}
-};
+
+	function clickHeaderTitle() {
+		uni.pageScrollTo({
+			scrollTop: 0
+		});
+	}
+
+	function jump(href) {
+		uni.createSelectorQuery()
+			.select('#box')
+			.boundingClientRect(bData => {
+				uni.createSelectorQuery()
+					.select(`#${href}`)
+					.boundingClientRect(data => {
+						let sctop = 0;
+						if (data.top < 0) {
+							if (bData.top < 0) {
+								sctop = data.top - bData.top;
+							} else {
+								sctop = bData.top - data.top;
+							}
+						} else {
+							if (bData.top < 0) {
+								sctop = data.top - bData.top;
+							} else {
+								sctop = bData.top + data.top;
+							}
+						}
+						uni.pageScrollTo({
+							scrollTop: sctop - 88
+						});
+					})
+					.exec();
+			})
+			.exec();
+	}
+
+	function toCity(id) {
+		uni.navigateTo({
+			url: `/pages/city/city?id=${id}`
+		});
+	}
+
+	const hotList = ref([])
+
+	function getHotCity() {
+		getHot().then(res => {
+			hotList.value = res.data;
+		});
+	}
+	const groupList = ref([])
+	const indexList = ref([])
+
+	function getGroup() {
+		console.log('开始请求', new Date().getTime());
+		getGroupCity().then(res => {
+			console.log('数据获取成功', new Date().getTime());
+			let group = [],
+				index = [];
+			for (let key in res.data) {
+				group.push({
+					title: key,
+					items: res.data[key]
+				});
+				index.push(key);
+			}
+			group.sort(function(a, b) {
+				return a.title.charCodeAt(0) - b.title.charCodeAt(0);
+			});
+			groupList.value = group;
+			console.log('数据处理完成', new Date().getTime());
+			index.sort();
+			indexList.value = index;
+		});
+	}
+	const curCity = ref('')
+
+	function getGuess() {
+		getGuessCity().then(res => {
+			curCity.value = res.data;
+		});
+	}
+	onMounted(() => {
+		getUser()
+		getHotCity()
+		getGroup()
+		getGuess()
+	})
 </script>
 
 <style>
-.city-title {
-	height: 36px;
-	line-height: 36px;
-	border-top: 2px solid #e4e4e4;
-	border-bottom: 1px solid #e4e4e4;
-	padding-left: 11px;
-}
-.city-item {
-	box-sizing: border-box;
-	height: 43px !important;
-	line-height: 43px;
-	text-align: center;
-	background-color: #fff;
-}
-.anchor-list {
-	position: fixed;
-	right: 0px;
-	top: 50%;
-	margin-top: -209px;
-	font-size: 14px;
-	width: 22px;
-	text-align: center;
-	color: #666;
-	background-color: #e4e4e4;
-	opacity: 0.5;
-	z-index: 99;
-}
-.bg-color-unset {
-	background-color: unset;
-}
-.border-t {
-	border-top: 1px solid #e4e4e4;
-}
-.cur-city {
-	border-top: 1px solid #e4e4e4;
-	border-bottom: 1px solid #e4e4e4;
-	margin-bottom: 10px;
-}
-.ellipsis {
-	white-space: nowrap;
-	overflow: hidden;
-	text-overflow: ellipsis;
-}
+	.city-title {
+		height: 36px;
+		line-height: 36px;
+		border-top: 2px solid #e4e4e4;
+		border-bottom: 1px solid #e4e4e4;
+		padding-left: 11px;
+	}
+
+	.city-item {
+		box-sizing: border-box;
+		height: 43px !important;
+		line-height: 43px;
+		text-align: center;
+		background-color: #fff;
+	}
+
+	.anchor-list {
+		position: fixed;
+		right: 0px;
+		top: 50%;
+		margin-top: -209px;
+		font-size: 14px;
+		width: 22px;
+		text-align: center;
+		color: #666;
+		background-color: #e4e4e4;
+		opacity: 0.5;
+		z-index: 99;
+	}
+
+	.bg-color-unset {
+		background-color: unset;
+	}
+
+	.border-t {
+		border-top: 1px solid #e4e4e4;
+	}
+
+	.cur-city {
+		border-top: 1px solid #e4e4e4;
+		border-bottom: 1px solid #e4e4e4;
+		margin-bottom: 10px;
+	}
+
+	.ellipsis {
+		white-space: nowrap;
+		overflow: hidden;
+		text-overflow: ellipsis;
+	}
 </style>
--- pages/login/login.vue
+++ pages/login/login.vue
@@ -1,19 +1,15 @@
 <template>
 	<view>
 		<view class="login-box">
-			<uni-easyinput :inputBorder="false" placeholder="账号" clearSize="18px" class="input-content border-b font-16" v-model="form.username"></uni-easyinput>
-			<uni-easyinput :inputBorder="false" placeholder="密码" class="input-content border-b font-16" type="password" v-model="form.password"></uni-easyinput>
+			<uni-easyinput :inputBorder="false" placeholder="账号" clearSize="18px" class="input-content border-b font-16"
+				v-model="form.username"></uni-easyinput>
+			<uni-easyinput :inputBorder="false" placeholder="密码" class="input-content border-b font-16" type="password"
+				v-model="form.password"></uni-easyinput>
 			<view class="uni-flex w-flex-cross-center">
-				<uni-easyinput
-					type="number"
-					:inputBorder="false"
-					placeholder="验证码"
-					clearSize="18px"
-					class="input-content font-16 flex-auto"
-					v-model="form.captcha_code"
-				></uni-easyinput>
+				<uni-easyinput type="number" :inputBorder="false" placeholder="验证码" clearSize="18px"
+					class="input-content font-16 flex-auto" v-model="form.captcha_code"></uni-easyinput>
 				<image :src="captcha" mode="scaleToFill" class="captcha"></image>
-				<view class="change-cap" @click="getCaptcha">
+				<view class="change-cap" @click="getCapt">
 					<text>看不清</text>
 					<br />
 					<text class="color-3b95e9">换一张</text>
@@ -22,116 +18,136 @@
 		</view>
 		<view><text class="login-tips">温馨提示：未注册过的账号，登录时将自动注册</text></view>
 		<view><text class="login-tips">注册过的用户可凭账号密码登录</text></view>
-		<button type="primary" class="login-btn" @click="login">登录</button>
+		<button type="primary" class="login-btn" @click="loginHandle">登录</button>
 		<view class="">
 			<navigator url="/pages/forget/forget"><text class="forget-pass">重置密码?</text></navigator>
 		</view>
 	</view>
 </template>
 
-<script>
-import { getCaptcha, login } from '@/common/fetch.js';
-export default {
-	data() {
-		return {
-			captcha: '',
-			form: {
-				username: '',
-				password: '',
-				captcha_code: ''
-			},
-		};
-	},
-	methods: {
-		getCaptcha() {
-			getCaptcha().then(res => {
-				this.captcha = res.data.code;
-			});
-		},
-		login() {
-			const popTxt = ''
-			if (this.form.username === '') {
-				popTxt = '请输入手机号/邮箱/用户名';
-			}
-			if (this.form.password === '') {
-				popTxt = '请输入密码';
-			}
-			if (this.form.captcha_code === '') {
-				popTxt = '请输入验证码';
-			}
-			if(popTxt) return this.$tipPop(popTxt)
-			login(this.form).then(res => {
-				if (!res.data.user_id) {
-					// 登录不成功
-					if (res.data.type === 'ERROR_CAPTCHA') this.getCaptcha();
-					return this.$tipPop(res.data.message)
-				} else {
-					uni.navigateBack();
-				}
-			});
-		}
-	},
-	created() {
-		this.getCaptcha();
+<script setup lang="ts">
+	import {
+		getCaptcha,
+		login,
+	} from '@/common/fetch.js';
+
+	import {
+		ref,
+		onMounted,
+		reactive,
+		getCurrentInstance,
+		ComponentInternalInstance
+	} from 'vue'
+
+	const captcha = ref("")
+
+	function getCapt() {
+		getCaptcha().then(res => {
+			captcha.value = res.data.code;
+		});
 	}
-};
+	const form = reactive({
+		username: '',
+		password: '',
+		captcha_code: '',
+	})
+
+	function loginHandle() {
+		const {
+			proxy
+		} = getCurrentInstance() as ComponentInternalInstance
+		console.log(45, app)
+		let popTxt = ''
+		if (form.username === '') {
+			popTxt = '请输入手机号/邮箱/用户名';
+		}
+		if (form.password === '') {
+			popTxt = '请输入密码';
+		}
+		if (form.captcha_code === '') {
+			popTxt = '请输入验证码';
+		}
+		if (popTxt) return proxy?.$tipPop(popTxt)
+		login(form).then(res => {
+			if (!res.data.user_id) {
+				// 登录不成功
+				if (res.data.type === 'ERROR_CAPTCHA') getCapt();
+				return proxy?.$tipPop(res.data.message)
+			} else {
+				uni.navigateBack();
+			}
+		});
+	}
+	onMounted(() => {
+		getCapt()
+	})
 </script>
 
 <style scoped>
-.input-content >>> .uni-easyinput__content {
-	height: 55px;
-}
-.border-b {
-	border-bottom: 1px solid #f1f1f1;
-}
-.login-box {
-	margin-top: 15px;
-	background-color: #fff;
-}
-.captcha {
-	width: 90px;
-	height: 40px;
-}
-.change-cap {
-	padding: 0 10px;
-	width: 65px;
-}
-.color-3b95e9 {
-	color: #3b95e9;
-}
+	.input-content>>>.uni-easyinput__content {
+		height: 55px;
+	}
 
-.font-16 >>> .uni-input-placeholder,
-.font-16 >>> .uni-input-input {
-	font-size: 16px;
-}
+	.border-b {
+		border-bottom: 1px solid #f1f1f1;
+	}
 
-.login-tips {
-	display: block;
-	font-size: 12px;
-	color: red;
-	padding: 8px 15px;
-}
-.login-btn {
-	margin: 0 10px;
-	background-color: #4cd964;
-}
-.forget-pass {
-	display: block;
-	margin-top: 20px;
-	margin-right: 10px;
-	color: #3b95e9;
-	text-align: right;
-}
-.flex-auto {
-	flex: 1 1 auto;
-}
-.pop-box {
-	padding: 10px 30px;
-	background-color: #fff;
-	border-radius: 6px 6px 2px 2px;
-}
-.pop-btn {
-	width: 100%;
-	background-color: #4cd964;
-}
+	.login-box {
+		margin-top: 15px;
+		background-color: #fff;
+	}
+
+	.captcha {
+		width: 90px;
+		height: 40px;
+	}
+
+	.change-cap {
+		padding: 0 10px;
+		width: 65px;
+	}
+
+	.color-3b95e9 {
+		color: #3b95e9;
+	}
+
+	.font-16>>>.uni-input-placeholder,
+	.font-16>>>.uni-input-input {
+		font-size: 16px;
+	}
+
+	.login-tips {
+		display: block;
+		font-size: 12px;
+		color: red;
+		padding: 8px 15px;
+	}
+
+	.login-btn {
+		margin: 0 10px;
+		background-color: #4cd964;
+	}
+
+	.forget-pass {
+		display: block;
+		margin-top: 20px;
+		margin-right: 10px;
+		color: #3b95e9;
+		text-align: right;
+	}
+
+	.flex-auto {
+		flex: 1 1 auto;
+	}
+
+	.pop-box {
+		padding: 10px 30px;
+		background-color: #fff;
+		border-radius: 6px 6px 2px 2px;
+	}
+
+	.pop-btn {
+		width: 100%;
+		background-color: #4cd964;
+	}
 </style>
