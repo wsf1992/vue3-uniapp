a3f7b7bc9515955b2e5f82fb344e2fc14db4c310
wsf1992 <741987899@qq.com>
2022-09-07 09:27:55 +0800

    fix: 获取城市分组接口去掉--响应太慢

--- pages/city/city.vue
+++ pages/city/city.vue
@@ -1,8 +1,14 @@
 <template>
 	<view>
 		<view class="city-form">
-			<uni-search-bar placeholder="输入学校 商务楼 地址" focus v-model="searchValue" @clear="(searchValue = ''), search()"
-				clearButton="auto" cancelButton="none"></uni-search-bar>
+			<uni-search-bar
+				placeholder="输入学校 商务楼 地址"
+				focus
+				v-model="searchValue"
+				@clear="(searchValue = ''), search()"
+				clearButton="auto"
+				cancelButton="none"
+			></uni-search-bar>
 			<button type="primary" class="save-btn" @click="search">提交</button>
 		</view>
 		<uni-list>
@@ -10,83 +16,72 @@
 				<uni-list-item :title="item.name" :note="item.address" clickable />
 			</template>
 		</uni-list>
-		<uni-card v-if="searchValue && !sList.length && !searchLoading" :is-shadow="false" is-full><text
-				class="uni-h6">很抱歉！无搜索结果</text></uni-card>
+		<uni-card v-if="searchValue && !sList.length && !searchLoading" :is-shadow="false" is-full><text class="uni-h6">很抱歉！无搜索结果</text></uni-card>
 	</view>
 </template>
 
 <script setup>
-	import {
-		getCity,
-		citySearch
-	} from '@/common/fetch.js';
-	import {
-		ref,
-		reactive,
-		onMounted
-	} from 'vue'
-	import {
-		onLoad
-	} from '@dcloudio/uni-app'
-	const cityId = ref('')
-	const searchValue = ref('')
-	const searchLoading = ref(false)
-	let cityObj = reactive({})
-	let sList = reactive([])
+import { getCity, citySearch } from '@/common/fetch.js';
+import { ref, reactive, onMounted } from 'vue';
+import { onLoad } from '@dcloudio/uni-app';
+const cityId = ref('');
+const searchValue = ref('');
+const searchLoading = ref(false);
+let cityObj = reactive({});
+let sList = ref([]);
 
-
-	function getCityMsg() {
-		uni.showNavigationBarLoading();
-		getCity({
-				cityId: cityId.value
-			})
-			.then(res => {
-				cityObj = res.data;
-				uni.setNavigationBarTitle({
-					title: cityObj.name
-				});
-			})
-			.finally(() => {
-				uni.hideNavigationBarLoading();
-			});
-	}
-
-	function search() {
-		searchLoading.value = true;
-		if (searchValue.value === '') return (sList = []);
-		citySearch({
-				city_id: cityId.value,
-				keyword: searchValue.value
-			})
-			.then(res => {
-				sList = res.data || [];
-			})
-			.finally(() => {
-				searchLoading.value = false;
-			});
-	}
-	onMounted(() => {
-		getCityMsg()
+function getCityMsg() {
+	uni.showNavigationBarLoading();
+	getCity({
+		cityId: cityId.value
 	})
+		.then(res => {
+			cityObj = res.data;
+			uni.setNavigationBarTitle({
+				title: cityObj.name
+			});
+		})
+		.finally(() => {
+			uni.hideNavigationBarLoading();
+		});
+}
 
-	onLoad(option => {
-		cityId.value = option.id;
-		getCityMsg();
+function search() {
+	searchLoading.value = true;
+	if (searchValue.value === '') return (sList.value = []);
+	citySearch({
+		city_id: cityId.value,
+		keyword: searchValue.value
 	})
+		.then(res => {
+			sList.value = res.data;
+		})
+		.finally(() => {
+			searchLoading.value = false;
+		});
+}
+onMounted(() => {
+	getCityMsg();
+});
+
+onLoad(option => {
+	cityId.value = option.id;
+	getCityMsg();
+});
 </script>
 
 <style scoped>
-	.city-form {
-		background-color: #fff;
-		border-top: 1px solid #e4e4e4;
-		border-bottom: 1px solid #e4e4e4;
-		margin-top: 10px;
-		padding-bottom: 10px;
-	}
+.city-form {
+	background-color: #fff;
+	border-top: 1px solid #e4e4e4;
+	border-bottom: 1px solid #e4e4e4;
+	margin-top: 10px;
+	padding-bottom: 10px;
+}
 
-	.save-btn {
-		margin: 0 10px;
-		height: 35px;
-		line-height: 35px;
-	}
+.save-btn {
+	margin: 0 10px;
+	height: 35px;
+	line-height: 35px;
+}
 </style>
--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,11 +1,8 @@
 <template>
 	<view class="w-flex-column" id="box">
-		<uni-nav-bar fixed backgroundColor="#3190e8" color="#fff" @clickTitle="clickHeaderTitle" status-bar
-			title="ele.me" leftWidth="120px" rightWidth="120px">
+		<uni-nav-bar fixed backgroundColor="#3190e8" color="#fff" @clickTitle="clickHeaderTitle" status-bar title="ele.me" leftWidth="120px" rightWidth="120px">
 			<template v-slot:left>
-				<navigator url="/pages/tabBar/profile/profile" v-if="user_id" open-type="switchTab">
-					<uni-icons type="person" size="25px" color="#fff"></uni-icons>
-				</navigator>
+				<navigator url="/pages/tabBar/profile/profile" v-if="user_id" open-type="switchTab"><uni-icons type="person" size="25px" color="#fff"></uni-icons></navigator>
 				<navigator url="/pages/login/login" v-else><text class="f-s-14">登录 | 注册</text></navigator>
 			</template>
 		</uni-nav-bar>
@@ -15,8 +12,7 @@
 				<text class="f-s-12 f-w-900 c-9f9f9f">定位不准时，请在城市列表中选择</text>
 			</template>
 		</uni-section>
-		<uni-section class="cur-city" :title="curCity.name" titleFontSize="18px" titleColor="#3190e8"
-			@click="toCity(curCity.id)">
+		<uni-section class="cur-city" :title="curCity.name" titleFontSize="18px" titleColor="#3190e8" @click="toCity(curCity.id)">
 			<template v-slot:right>
 				<text class="c-9f9f9f">></text>
 			</template>
@@ -35,8 +31,7 @@
 		</view>
 		<template v-for="item in groupList" :key="item.title">
 			<view class="w-flex-column mar-b-10">
-				<uni-section class="border-t" :id="item.title" :title="item.title" titleFontSize="14px"
-					titleColor="#666"></uni-section>
+				<uni-section class="border-t" :id="item.title" :title="item.title" titleFontSize="14px" titleColor="#666"></uni-section>
 				<uni-grid :column="4" borderColor="#e4e4e4" :square="false">
 					<template v-for="(item, i) in item.items" :key="i">
 						<uni-grid-item class="city-item">
@@ -55,165 +50,157 @@
 </template>
 
 <script setup>
-	import {
-		getGuessCity,
-		getHot,
-		getGroupCity,
-		getUserInfo
-	} from '@/common/fetch.js';
+import { getGuessCity, getHot, getGroupCity, getUserInfo } from '@/common/fetch.js';
+import initCityData from '@/common/cities.js';
+import { ref, onMounted } from 'vue';
 
-	import {
-		ref,
-		onMounted
-	} from 'vue'
+const user_id = ref('');
 
-	const user_id = ref('')
+function getUser() {
+	const local_user_id = uni.getStorageSync('user_id');
+	getUserInfo(local_user_id).then(res => {
+		console.log(123, res);
+		if (res.data.user_id) user_id.value = res.data.user_id;
+	});
+}
 
-	function getUser() {
-		const local_user_id = uni.getStorageSync('user_id');
-		getUserInfo(local_user_id).then(res => {
-			console.log(123, res);
-			if (res.data.user_id) user_id.value = res.data.user_id;
-		});
-	}
+function clickHeaderTitle() {
+	uni.pageScrollTo({
+		scrollTop: 0
+	});
+}
 
-	function clickHeaderTitle() {
-		uni.pageScrollTo({
-			scrollTop: 0
-		});
-	}
-
-	function jump(href) {
-		uni.createSelectorQuery()
-			.select('#box')
-			.boundingClientRect(bData => {
-				uni.createSelectorQuery()
-					.select(`#${href}`)
-					.boundingClientRect(data => {
-						let sctop = 0;
-						if (data.top < 0) {
-							if (bData.top < 0) {
-								sctop = data.top - bData.top;
-							} else {
-								sctop = bData.top - data.top;
-							}
+function jump(href) {
+	uni.createSelectorQuery()
+		.select('#box')
+		.boundingClientRect(bData => {
+			uni.createSelectorQuery()
+				.select(`#${href}`)
+				.boundingClientRect(data => {
+					let sctop = 0;
+					if (data.top < 0) {
+						if (bData.top < 0) {
+							sctop = data.top - bData.top;
 						} else {
-							if (bData.top < 0) {
-								sctop = data.top - bData.top;
-							} else {
-								sctop = bData.top + data.top;
-							}
+							sctop = bData.top - data.top;
 						}
-						uni.pageScrollTo({
-							scrollTop: sctop - 88
-						});
-					})
-					.exec();
-			})
-			.exec();
-	}
+					} else {
+						if (bData.top < 0) {
+							sctop = data.top - bData.top;
+						} else {
+							sctop = bData.top + data.top;
+						}
+					}
+					uni.pageScrollTo({
+						scrollTop: sctop - 88
+					});
+				})
+				.exec();
+		})
+		.exec();
+}
 
-	function toCity(id) {
-		uni.navigateTo({
-			url: `/pages/city/city?id=${id}`
+function toCity(id) {
+	uni.navigateTo({
+		url: `/pages/city/city?id=${id}`
+	});
+}
+
+const hotList = ref([]);
+
+function getHotCity() {
+	getHot().then(res => {
+		hotList.value = res.data;
+	});
+}
+const groupList = ref([]);
+const indexList = ref([]);
+
+function getGroup() {
+	// console.log('开始请求', new Date().getTime());
+	// getGroupCity().then(res => {
+	// 	console.log('数据获取成功', new Date().getTime());
+	let group = [],
+		index = [];
+	for (let key in initCityData) {
+		group.push({
+			title: key,
+			items: initCityData[key]
 		});
+		index.push(key);
 	}
+	group.sort(function(a, b) {
+		return a.title.charCodeAt(0) - b.title.charCodeAt(0);
+	});
+	groupList.value = group;
+	// console.log('数据处理完成', new Date().getTime());
+	index.sort();
+	indexList.value = index;
+	// });
+}
+const curCity = ref('');
 
-	const hotList = ref([])
-
-	function getHotCity() {
-		getHot().then(res => {
-			hotList.value = res.data;
-		});
-	}
-	const groupList = ref([])
-	const indexList = ref([])
-
-	function getGroup() {
-		console.log('开始请求', new Date().getTime());
-		getGroupCity().then(res => {
-			console.log('数据获取成功', new Date().getTime());
-			let group = [],
-				index = [];
-			for (let key in res.data) {
-				group.push({
-					title: key,
-					items: res.data[key]
-				});
-				index.push(key);
-			}
-			group.sort(function(a, b) {
-				return a.title.charCodeAt(0) - b.title.charCodeAt(0);
-			});
-			groupList.value = group;
-			console.log('数据处理完成', new Date().getTime());
-			index.sort();
-			indexList.value = index;
-		});
-	}
-	const curCity = ref('')
-
-	function getGuess() {
-		getGuessCity().then(res => {
-			curCity.value = res.data;
-		});
-	}
-	onMounted(() => {
-		getUser()
-		getHotCity()
-		getGroup()
-		getGuess()
-	})
+function getGuess() {
+	getGuessCity().then(res => {
+		curCity.value = res.data;
+	});
+}
+onMounted(() => {
+	getUser();
+	getHotCity();
+	getGroup();
+	getGuess();
+});
 </script>
 
 <style>
-	.city-title {
-		height: 36px;
-		line-height: 36px;
-		border-top: 2px solid #e4e4e4;
-		border-bottom: 1px solid #e4e4e4;
-		padding-left: 11px;
-	}
+.city-title {
+	height: 36px;
+	line-height: 36px;
+	border-top: 2px solid #e4e4e4;
+	border-bottom: 1px solid #e4e4e4;
+	padding-left: 11px;
+}
 
-	.city-item {
-		box-sizing: border-box;
-		height: 43px !important;
-		line-height: 43px;
-		text-align: center;
-		background-color: #fff;
-	}
+.city-item {
+	box-sizing: border-box;
+	height: 43px !important;
+	line-height: 43px;
+	text-align: center;
+	background-color: #fff;
+}
 
-	.anchor-list {
-		position: fixed;
-		right: 0px;
-		top: 50%;
-		margin-top: -209px;
-		font-size: 14px;
-		width: 22px;
-		text-align: center;
-		color: #666;
-		background-color: #e4e4e4;
-		opacity: 0.5;
-		z-index: 99;
-	}
+.anchor-list {
+	position: fixed;
+	right: 0px;
+	top: 50%;
+	margin-top: -209px;
+	font-size: 14px;
+	width: 22px;
+	text-align: center;
+	color: #666;
+	background-color: #e4e4e4;
+	opacity: 0.5;
+	z-index: 99;
+}
 
-	.bg-color-unset {
-		background-color: unset;
-	}
+.bg-color-unset {
+	background-color: unset;
+}
 
-	.border-t {
-		border-top: 1px solid #e4e4e4;
-	}
+.border-t {
+	border-top: 1px solid #e4e4e4;
+}
 
-	.cur-city {
-		border-top: 1px solid #e4e4e4;
-		border-bottom: 1px solid #e4e4e4;
-		margin-bottom: 10px;
-	}
+.cur-city {
+	border-top: 1px solid #e4e4e4;
+	border-bottom: 1px solid #e4e4e4;
+	margin-bottom: 10px;
+}
 
-	.ellipsis {
-		white-space: nowrap;
-		overflow: hidden;
-		text-overflow: ellipsis;
-	}
+.ellipsis {
+	white-space: nowrap;
+	overflow: hidden;
+	text-overflow: ellipsis;
+}
 </style>
